a3b17c25654b9df2d3f0f5307f3b91ea7a1e4575
Pavan Boro <Pavan.Boro@resideo.com>
2021-06-26 02:11:59 +0530

    added basic starter server.js template

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+
+// Initialize Express app
+const app = express()
+// Set  PORT 
+const PORT = process.env.PORT || 3000; // process.env is made available by dotenv. Stores env constants in .env file 
+
+
+// Make public folder accessible to client-side
+app.use(express.static('public'))
+
+//===== Set Template Engine ==========
+app.use(expressLayout)
+app.set('views',path.join(__dirname, 'resources/views')) // sets views
+app.set('view engine', 'ejs')  // Tell app to use ejs template engine
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
